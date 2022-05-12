0dbed6d87549c1f666a07e741fc191173d7dd694
iwona <iwo.jarek@gmail.com>
2022-05-12 17:35:28 +0200

    page not found

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,14 +3,12 @@ import styles from './NavBar.module.scss';
 const NavBar = () => {
   return (
     <nav className={styles.navBar}>
-      <div className={styles.container}>
-        <span className="fa fa-tasks"></span>
-          <ul className={styles.navigation}>
-            <li><a href='/'>Home</a></li>
-            <li><a href='/favorite'>Favorite</a></li>
-            <li><a href='/about'>About</a></li>
-          </ul>
-      </div>
+      <p className={styles.icon}><span className="fa fa-tasks"></span></p>
+      <ul className={styles.navigation}>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/favorite'>Favorite</a></li>
+        <li><a href='/about'>About</a></li>
+      </ul>
     </nav>
   )
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import styles from './NotFound.module.scss';
+
+const NotFound = () => {
+  return (
+    <div className={styles.title}>
+      <h1>There's nothing here!</h1>
+    </div>
+  )
+};
+
+export default NotFound;
